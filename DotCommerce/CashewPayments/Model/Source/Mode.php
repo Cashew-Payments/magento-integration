1fb64a2b168bd4c46ffe663465053ab0ce2d82d4
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-01-26 16:02:16 +0400

    add environment mode

---
+++ DotCommerce/CashewPayments/Model/Source/Mode.php
@@ -0,0 +1,33 @@
+<?php
+/*
+ * @category    Spotii
+ * @package     Spotii_Spotiipay
+ * @copyright   Copyright (c) Spotii (https://www.spotii.me/)
+ */
+
+namespace DotCommerce\CashewPayments\Model\Source;
+
+/**
+ * Class Mode
+ * @package DotCommerce\CashewPayments\Model\Source
+ */
+class Mode implements \Magento\Framework\Option\ArrayInterface
+{
+
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            [
+                'value' => 'https://api.cashewpayments.com/v1/',
+                'label' => 'Production',
+            ],
+            [
+                'value' => 'https://api-sandbox.cashewpayments.com/v1/',
+                'label' => 'Sandbox',
+            ]
+        ];
+    }
+}
