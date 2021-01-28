09086965d400944d2a8b9d7d92be0fb6d675fe2c
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-01-28 12:15:49 +0400

    Add check for currency and language

--- DotCommerce/CashewPayments/Model/Cashew.php
+++ DotCommerce/CashewPayments/Model/Cashew.php
@@ -71,7 +71,8 @@ class Cashew extends AbstractMethod
         if ($quote && ($quote instanceof MagentoQuote)) {
             $orderSubtotal = $quote->getSubtotal();
             $currency = $quote->getCurrency()->getCurrencyCode();
-            $this->logger->debug('CURRENCY'.$currency);
+            $this->logger->error('CURRENCY'.$currency.' '.$quote->getBaseCurrencyCode());
+            $this->logger->debug('CURRENCY'.$quote->getBaseCurrencyCode());
             $minOrderTotal = !empty($this->config->getMinimumOrderTotal()) ? $this->config->getMinimumOrderTotal() : 0;
             $maxOrderTotal = !empty($this->config->getMaximumOrderTotal()) ? $this->config->getMaximumOrderTotal() : PHP_INT_MAX;
             if ($orderSubtotal >= $minOrderTotal && $orderSubtotal <= $maxOrderTotal) {
