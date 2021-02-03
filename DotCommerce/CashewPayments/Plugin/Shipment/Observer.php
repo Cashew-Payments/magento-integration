f63d08c2a8799032d2b4b5a03e2a331d32e3c496
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-02-03 13:10:29 +0400

    add observer for shipment

--- DotCommerce/CashewPayments/Plugin/Shipment/Observer.php
+++ DotCommerce/CashewPayments/Plugin/Shipment/Observer.php
@@ -5,43 +5,38 @@ namespace DotCommerce\CashewPayments\Plugin\Shipment;
 use Magento\Framework\Event\ObserverInterface;
 
 use DotCommerce\CashewPayments\Helper\Api as ApiHelper;
-
-use Psr\Log\LoggerInterface;
-class Observer {
+class Observer implements ObserverInterface
+{
     const API_POST = 'refunds/merchant';
 
-    /**
-     * Param to save api helper
-     * 
-     * @var ApiHelper
-     */
     protected $apiHelper;
     protected $logger;
-    
-    public function __construct( 
-        ApiHelper $apiHelper,
-        LoggerInterface $logger
-    ) {
-        $this->apiHelper = $apiHelper;
-        $this->logger = $logger;
-    }
-    public function eventLogging(\Magento\Framework\Object $observer) {
-        $shipment = $observer->getEvent()->getShipment();
-        $order = $shipment->getOrder();
-        $cashewPayment  = $order->getPayment()->getMethod();
-        $this->logger->debug('Shippment');
-        if ($cashewPayment == 'cashewpayment') {
-            $token = $this->apiHelper->getToken();
-            $data = [
-                'orderStatus' => 'DISPATCHED'
-            ];
-            $response = $this->apiHelper
-                ->postData($token, json_encode($data), self::API_POST);
 
-            if ($response['status'] !== 'success') {
-                $this->logger->debug('Dispatch Errors:');
-                $this->logger->debug(print_r(json_encode($response), true));
-            }
+  public function __construct(ApiHelper $apiHelper,
+  Psr\Log\LoggerInterface $logger)
+  {
+    $this->apiHelper = $apiHelper;
+    $this->logger = $logger;
+  }
+
+  public function execute(\Magento\Framework\Event\Observer $observer)
+  {
+    $shipment = $observer->getEvent()->getShipment();
+    $order = $shipment->getOrder();
+    $cashewPayment  = $order->getPayment()->getMethod();
+    $this->logger->debug('Shippment');
+    if ($cashewPayment == 'cashewpayment') {
+        $token = $this->apiHelper->getToken();
+        $data = [
+            'orderStatus' => 'DISPATCHED'
+        ];
+        $response = $this->apiHelper
+            ->postData($token, json_encode($data), self::API_POST);
+
+        if ($response['status'] !== 'success') {
+            $this->logger->debug('Dispatch Errors:');
+            $this->logger->debug(print_r(json_encode($response), true));
         }
     }
+  }
 }
