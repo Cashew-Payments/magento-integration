cec75457ea5a3bff4c242c63d289033f2d97d241
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-02-07 13:15:24 +0400

    .

--- DotCommerce/CashewPayments/Plugin/Creditmemo/Refund.php
+++ DotCommerce/CashewPayments/Plugin/Creditmemo/Refund.php
@@ -91,7 +91,7 @@ class Refund
         $refundedOrder  = $subject->get($orderId)->getOrder();
         $refundedAmount = $subject->get($orderId)->getGrandTotal();
         $cashewPayment  = $refundedOrder->getPayment()->getMethod();
-        $this->logger->debug(print_r($subject->getOrderItemId()));
+        $this->logger->debug(print_r($result->getOrderItemId()));
         if ($cashewPayment == 'cashewpayment') {
             $data = [
                 'orderReference' => $orderId,
