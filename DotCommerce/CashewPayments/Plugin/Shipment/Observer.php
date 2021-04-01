a9429e29e2b5dd92a0ff29181394fc05ea3dfe77
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-04-01 14:41:23 +0400

    output items

--- DotCommerce/CashewPayments/Plugin/Shipment/Observer.php
+++ DotCommerce/CashewPayments/Plugin/Shipment/Observer.php
@@ -32,12 +32,11 @@ class Observer implements ObserverInterface
         $this->logger->debug('Entity ID: ' . $order->getEntityId());
         $this->logger->debug('Payment Method: ' . $cashewPayment);
         $this->logger->debug('Shippments count: ' . $order->getShipmentsCollection()->count());
-        // $this->logger->debug('Object: ' . var_dump($shipment->getAllItems()));
-        $this->logger->debug('Collection: ' . var_dump($shipment->getItemsCollection()));
 
         $shipmentObj = $observer->getEvent()->getShipment();
 
         foreach ($shipmentObj->getItemsCollection() as $orderItem) {
+            $this->logger->debug('Collection: ' . var_dump($orderItem));
             if (!$orderItem->getParentItem()) {
                 $qty = $orderItem->getQtyOrdered();
                 $this->logger->debug('Max: ' . $orderItem->getTotalQty());
