ef62e11155cdbb13d7aa1ec651e5da365b4a570d
Gonçalo Silva Dias <goncalo@cashewpayments.com>
2021-04-01 14:01:32 +0400

    add entity

--- DotCommerce/CashewPayments/Plugin/Shipment/Observer.php
+++ DotCommerce/CashewPayments/Plugin/Shipment/Observer.php
@@ -37,7 +37,7 @@ class Observer implements ObserverInterface
         foreach ($shipmentObj->getItemsCollection() as $orderItem) {    
             if (!$orderItem->getParentItem()) {
                 $qty = $orderItem->getQtyOrdered();
-                $this->logger->debug('Max: '.max($orderItem->getQtyShipped(), $orderItem->getQtyInvoiced()));
+                $this->logger->debug('Max: '.$orderItem->getQtyShipped().' '. $orderItem->getQtyInvoiced());
                 if (!$order->getReordered()) {
                     $qty -= max($orderItem->getQtyShipped(), $orderItem->getQtyInvoiced());
                 }
